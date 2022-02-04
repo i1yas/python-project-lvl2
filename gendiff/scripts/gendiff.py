d32e321532205c56a76a2ca7645f405a65573d35
ilyas <ilyasers@gmail.com>
2022-02-04 14:41:12 +0300

    feat: add --format option for gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,8 @@ def main():
 
     parser.add_argument('first_line', type=str)
     parser.add_argument('second_line', type=str)
+    parser.add_argument('-f', '--format', dest='FORMAT', type=str,
+                        help='set format of output')
 
     args = parser.parse_args()
     print(args.accumulate(args.integers))
