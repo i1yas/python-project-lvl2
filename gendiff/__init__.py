dd7513ab315a74d6fb9db75386ae483587a97c6e
ilyas <ilyasers@gmail.com>
2022-02-05 18:31:18 +0300

    feat: add support for nested files

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -14,6 +14,15 @@ def get_diff(old, new):
     for key in all_keys_in_order:
         old_value = old.get(key)
         new_value = new.get(key)
+        is_nested = isinstance(old_value, dict) and isinstance(new_value, dict)
+
+        if is_nested:
+            diff.append({
+                'type': 'keep',
+                'key': key,
+                'nested': get_diff(old_value, new_value)
+            })
+            continue
 
         if old_value == new_value:
             diff.append({
@@ -40,33 +49,83 @@ def get_diff(old, new):
     return diff
 
 
-def format_value(value):
+symbol_mapping = {
+    'remove': '-',
+    'add': '+',
+    'keep': ' '
+}
+
+
+def get_tab(symbol_type, depth):
+    space = ' '
+    symbol = symbol_mapping[symbol_type]
+    tab = space * 4 * depth + space * 2 + symbol + space
+    return tab
+
+
+def format_key_value(key, value, symbol_type, depth):
+    tab = get_tab(symbol_type, depth)
+    return f'{tab}{key}: {value}'
+
+
+def wrap_with_braces(content, depth):
+    space = ' '
+    return '\n'.join([
+        '{',
+        content,
+        space * 4 * depth + '}'
+    ])
+
+
+def format_value(value, depth):
     if isinstance(value, str):
         return value
+    if isinstance(value, dict):
+        lines = map(
+            lambda item: format_key_value(
+                key=item[0],
+                value=format_value(item[1], depth + 1),
+                symbol_type='keep',
+                depth=depth
+            ),
+            value.items())
+        content = '\n'.join(list(lines))
+        return wrap_with_braces(content, depth)
+
     return json.dumps(value)
 
 
 def format_diff(diff):
-    symbol_mapping = {
-        'remove': '-',
-        'add': '+',
-        'keep': ' '
-    }
-    result = []
-    tab = '  '
 
-    for line in diff:
-        type = line['type']
-        symbol = symbol_mapping[type]
-        key = line['key']
-        value = line['value']
-        result.append(f'{tab}{symbol} {key}: {format_value(value)}')
+    def walk(diff, depth):
+        result = []
 
-    return '\n'.join([
-        '{',
-        *result,
-        '}'
-    ])
+        for line in diff:
+            type = line['type']
+            key = line['key']
+            value = line.get('value')
+            nested = line.get('nested')
+
+            formatted_value = ''
+            if nested:
+                formatted_value = walk(nested, depth + 1)
+            else:
+                formatted_value = format_value(
+                    value,
+                    depth=depth + 1
+                )
+
+            result.append(format_key_value(
+                key=key,
+                value=formatted_value,
+                symbol_type=type,
+                depth=depth
+            ))
+
+        content = '\n'.join(result)
+        return wrap_with_braces(content, depth)
+
+    return walk(diff, 0)
 
 
 def generate_diff(filepath1, filepath2):
